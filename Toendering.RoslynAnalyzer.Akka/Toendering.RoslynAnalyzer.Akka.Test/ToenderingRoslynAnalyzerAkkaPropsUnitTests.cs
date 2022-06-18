61ef91f2eee4bb4bfc8f72ad4b11b51e8515ae5a
Janus Tøndering <janus@toendering.dk>
2022-06-18 22:15:24 +0200

    Initial commit.

---
+++ Toendering.RoslynAnalyzer.Akka/Toendering.RoslynAnalyzer.Akka.Test/ToenderingRoslynAnalyzerAkkaPropsUnitTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using VerifyCS = Toendering.RoslynAnalyzer.Akka.Test.CSharpAnalyzerVerifier<
+    Toendering.RoslynAnalyzer.Akka.ToenderingRoslynAnalyzerAkkaPropsAnalyzer>;
+
+namespace Toendering.RoslynAnalyzer.Akka.Test
+{
+    [TestClass]
+    public class ToenderingRoslynAnalyzerAkkaPropsUnitTest
+    {
+        ReferenceAssemblies _referenceAssemblies = ReferenceAssemblies.Default
+            .AddPackages(ImmutableArray.Create(
+                new PackageIdentity("Akka", "1.4.39")
+                ))
+            .AddAssemblies(ImmutableArray.Create(
+                "Akka"
+            ));
+
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+using Akka.Actor;
+
+class TestActor : ReceiveActor
+{
+    public TestActor(int affinity, string name, bool b1) {}
+    public TestActor(int affinity, string name) { }
+
+    public static Props CreateProps() => 
+        {|TRD001:Props.Create<TestActor>(0, false, false)|};
+}
+";
+            var spec = new VerifyCS.Test
+            {
+
+                ReferenceAssemblies = _referenceAssemblies,
+                TestState =
+                {
+                    Sources = { test }
+                }
+            };
+
+            await spec.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task TestMethod3()
+        {
+            var test = @"
+using Akka.Actor;
+
+class TestActor : ReceiveActor
+{
+    public TestActor(int affinity, string name, bool b1) {}
+    public TestActor(int affinity, string name) { }
+    public TestActor(int affinity, bool b0, bool b1) {}
+
+    public static Props CreateProps() => 
+        Props.Create<TestActor>(0, false, false);
+}
+";
+            var spec = new VerifyCS.Test
+            {
+
+                ReferenceAssemblies = _referenceAssemblies,
+                TestState =
+                {
+                    Sources = { test }
+                }
+            };
+
+            await spec.RunAsync();
+
+        }
+
+        [TestMethod]
+        public async Task TestMethod4()
+        {
+            var test = @"
+using Akka.Actor;
+
+class TestActor : ReceiveActor
+{
+    private TestActor(int affinity) {}
+
+    public static Props CreateProps() => 
+        {|TRD001:Props.Create<TestActor>(0)|};
+}
+";
+            var spec = new VerifyCS.Test
+            {
+
+                ReferenceAssemblies = _referenceAssemblies,
+                TestState =
+                {
+                    Sources = { test }
+                }
+            };
+
+            await spec.RunAsync();
+        }
+
+    }
+}
---
+++ Toendering.RoslynAnalyzer.Akka/Toendering.RoslynAnalyzer.Akka/ToenderingRoslynAnalyzerAkkaPropsAnalyzer.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Toendering.RoslynAnalyzer.Akka
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ToenderingRoslynAnalyzerAkkaPropsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "TRD001";
+
+        private static readonly LocalizableString _title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString _messageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString _description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string _category = "Usage";
+
+        private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor(DiagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(_rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeInvocationExpression, SyntaxKind.InvocationExpression);
+
+        }
+
+        private void AnalyzeInvocationExpression(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is InvocationExpressionSyntax invocationExpression
+                && invocationExpression.Expression is MemberAccessExpressionSyntax memberAccess
+                && memberAccess.Expression is IdentifierNameSyntax identifier)
+            {
+                TypeInfo typeInfo = context.SemanticModel.GetTypeInfo(context.Node);
+                ITypeSymbol type = typeInfo.Type;
+
+                ITypeSymbol[] targetTypes = invocationExpression.ArgumentList.Arguments
+                    .Select(x => context.SemanticModel.GetTypeInfo(x.Expression).Type)
+                    .ToArray();
+
+                if (type.ContainingAssembly.Identity.Name == "Akka" 
+                    && identifier.Identifier.Text == "Props"
+                    && memberAccess.Name is GenericNameSyntax genericName 
+                    && genericName.TypeArgumentList.Arguments.Any())
+                {
+
+                    if (genericName.TypeArgumentList.Arguments.Count == 1)
+                    {
+                        TypeSyntax targetTypeSyntax = genericName.TypeArgumentList.Arguments[0];
+                        INamedTypeSymbol candidateType = context.SemanticModel.GetTypeInfo(targetTypeSyntax).Type as INamedTypeSymbol;
+
+                        bool hasMatchingCtor = candidateType.Constructors
+                            .Where(c => c.DeclaredAccessibility == Accessibility.Public)
+                            .Any(method => MatchesMethodSignature(context.Compilation, method, targetTypes));
+
+                        if (hasMatchingCtor)
+                            return;
+
+                        Diagnostic diagnostic = Diagnostic.Create(_rule,
+                            invocationExpression.GetLocation(),
+                            candidateType.Name);
+
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+            }
+        }
+
+        private bool MatchesMethodSignature(Compilation compilation, IMethodSymbol methodSymbol, IList<ITypeSymbol> args)
+        {
+            ITypeSymbol[] targetArgs = methodSymbol.Parameters.Select(x => x.Type).ToArray();
+            if (args.Count != targetArgs.Length)
+                return false;
+
+            for (int i = 0; i < targetArgs.Length; i++)
+            {
+                ITypeSymbol sourceType = args[i];
+                ITypeSymbol destinationType = targetArgs[i];
+
+                if (sourceType.Equals(destinationType, SymbolEqualityComparer.Default))
+                    continue;
+                
+                Conversion conversion = compilation.ClassifyConversion(sourceType, destinationType);
+                if (conversion.Exists)
+                    continue;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
